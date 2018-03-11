7eab90879d49ccb435248e77b64bcec0b491b27a
Lance Valera <lance.valera89@gmail.com>
2018-03-11 14:47:16 -0400

    added refs and on submit function

--- imports/ui/Login/loginForm.js
+++ imports/ui/Login/loginForm.js
@@ -11,19 +11,23 @@ const style = {
 // App component - represents the whole app
 export default class LoginForm extends Component {
 
+  handleLogin(){
+    this.props.onLogin();
+  }
+
   render() {
     return (
-        <form className="form" method="" action="">
+        <form className="form" onSubmit={this.handleSignIn.bind(this)}>
           <div className="content" style={{margin: 'auto'}}>
             <div className="input-group form-group-no-border input-lg">
-              <input type="text" placeholder="Username" className="form-control" style={{color: 'white'}} />
+              <input type="text" ref="username" placeholder="Username" className="form-control" style={{color: 'white'}} />
             </div>
             <div className="input-group form-group-no-border input-lg">
-              <input type="text" placeholder="Password" className="form-control" style={{color: 'white'}} />
+              <input type="text" ref="password" placeholder="Password" className="form-control" style={{color: 'white'}} />
             </div>
           </div>
           <div className="footer text-center submitbtn">
-              <button className="btn btn-primary btn-round bluebtn" style={{width: '100%'}}>Login</button>
+              <button type="submit" className="btn btn-primary btn-round bluebtn" style={{width: '100%'}} >Login</button>
           </div>
           <div className='pull-left'>
               <a className='nav-link' style={{color: 'white'}}><span> Create Account </span></a>
