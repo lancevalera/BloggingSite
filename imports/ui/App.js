bb19d17749ef5550c0e75fa75c619a0273f8a1d7
Lance Valera <lance.valera89@gmail.com>
2018-03-11 14:46:03 -0400

    added constructor, started login logic

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -11,23 +11,34 @@ import Profile from './Profile/profile.js';
 
 // App component - represents the whole app
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: null
+    }
+  }
+
+  login(username, password){
+    this.setState({
+      user: true
+    })
+  }
 
   render() {
     return (
     <div>
+    <BrowserRouter>
       <div className=''>
-        <Navbar />
-      </div>
-      <div className=''>
-        <BrowserRouter>
+        <Navbar isLoggedIn={this.state.user}/>
           <Switch>
-            <Route exact path='/' component={Login}/>
-            <Route path='/login' component={Login}/>
+            <Route exact path='/' render={(props) => <Home something="hi"/>}/>
+            <Route path='/login' render={(props) => <Login something="hi" onLogin={this.login.bind(this)}/>}/>
             <Route path='/home' component={Home}/>
             <Route path='/profile' component={Profile}/>
           </Switch>
-          </BrowserRouter>
       </div>
+      </BrowserRouter>
     </div>
     );
   }
