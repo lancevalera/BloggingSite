7e7102f5536a1106aad4cb0cc5a4e3ee4219c9a4
Lance Valera <lance.valera89@gmail.com>
2018-03-12 21:48:39 -0400

    added props

--- imports/ui/Home/card.js
+++ imports/ui/Home/card.js
@@ -5,12 +5,13 @@ import '../../now-ui-kit.css';
 export default class Cards extends Component {
 
   render() {
+    console.log(this.props);
     return (
       <div className='card'>
         <div className='card-body'>
-          <h5 className="card-title">Card title</h5>
-          <p className='card-text description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <p className='card-text author'> Posted By: </p>
+          <h5 className="card-title">{this.props.body.title}</h5>
+          <p className='card-text description'>{this.props.body.header}</p>
+          <p className='card-text author'> {this.props.body.author} </p>
         </div>
       </div>
     );
