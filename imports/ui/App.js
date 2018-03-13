1059b770161308ed4d41145f51a2d8c9ff0c9477
Lance Valera <lance.valera89@gmail.com>
2018-03-12 21:48:28 -0400

    added imports for browserHistory and User, fleshed out login logic

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { Switch, Route } from 'react-router-dom'
+import { browserHistory } from 'react-router'
 
 import '../now-ui-kit.css';
 
@@ -8,6 +9,8 @@ import Login from './Login/login.js';
 import Navbar from './Navbar/navbar.js';
 import Home from './Home/home.js';
 import Profile from './Profile/profile.js';
+
+import { Users } from '../api/users.js';
 
 // App component - represents the whole app
 export default class App extends Component {
@@ -19,23 +22,32 @@ export default class App extends Component {
     }
   }
 
-  login(username, password){
-    this.setState({
-      user: true
-    })
+  login(uname, pword){
+    var valUser = Users.find({username: uname, password: pword}).fetch()
+    if(valUser.length > 0){
+      this.setState({user: {
+        username: valUser[0].username,
+        password: valUser[0].password,
+        firstname: valUser[0].fname,
+        lastname: valUser[0].lname
+        }
+      })
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
     return (
     <div>
-    <BrowserRouter>
+    <BrowserRouter history={browserHistory}>
       <div className=''>
         <Navbar isLoggedIn={this.state.user}/>
           <Switch>
-            <Route exact path='/' render={(props) => <Home something="hi"/>}/>
-            <Route path='/login' render={(props) => <Login something="hi" onLogin={this.login.bind(this)}/>}/>
-            <Route path='/home' component={Home}/>
-            <Route path='/profile' component={Profile}/>
+            <Route exact path='/' render={(props) => <Home router={props} user={this.state.user} />}/>
+            <Route path='/login' render={(props) => <Login router={props} onLogin={this.login.bind(this)} />}/>
+            <Route path='/profile' render={(props) => <Profile router={props} user={this.state.user} />} />
           </Switch>
       </div>
       </BrowserRouter>
