7d27ae437c759391f7d8e18ac0ba75789163d034
Lance Valera <lance.valera89@gmail.com>
2018-03-13 23:15:50 -0400

    added css class

--- imports/ui/Home/card.js
+++ imports/ui/Home/card.js
@@ -8,7 +8,7 @@ export default class Cards extends Component {
     console.log(this.props);
     return (
       <div className='card home-card'>
-        <div className='card-body'>
+        <div className='card-body changemin'>
           <h5 className="card-title">{this.props.body.title}</h5>
           <p className='card-text description'>{this.props.body.header}</p>
           <p className='card-text author'> {this.props.body.author} </p>
