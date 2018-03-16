d6885414c02f54b0ae2d3be4b383ed5aa06ccc9f
Lance Valera <lancevalera@Lances-MacBook-Pro.local>
2018-03-16 14:48:35 -0400

    added imports, server call and render methods

--- imports/ui/BlogPost/blogpost.js
+++ imports/ui/BlogPost/blogpost.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
 
 import '../../now-ui-kit.css';
 import './blogpost.css';
 
 import Header from '../Header/header.js';
-
-import { Posts } from '../../api/posts.js';
+import PostContent from './postcontent.js';
+import UpdateDelete from './updatedelete.js';
 
 export default class BlogPost extends Component {
   constructor(props){
@@ -17,9 +18,32 @@ export default class BlogPost extends Component {
   }
 
   componentWillMount(){
+    Meteor.call('posts.getById', this.props.router.match.params.postId, (err, result) => {
+      if(err)
+        console.log(err)
+      else
+        this.setState({post: result[0]});
+    });
     console.log(this.props);
-    var postValue = Posts.find({_id: this.props.router.match.params.postId}).fetch();
-    this.setState({post: postValue[0]})
+  }
+
+  renderPost() {
+    if(this.state.post)
+      return <PostContent body={this.state.post.body} header={this.state.post.header} author={this.state.post.author}
+              title={this.state.post.title} />
+    else
+      return null;
+  }
+
+  renderUpdateDelete() {
+    if(this.state.post && this.props.user){
+      if(this.state.post.authorID == this.props.user._id)
+        return (<div> <UpdateDelete /> </div>);
+      else
+        return null;
+    } else {
+      return null;
+    }
   }
 
   render() {
@@ -27,20 +51,9 @@ export default class BlogPost extends Component {
     return (
       <div>
         <Header image='/images/profile.jpeg' textValue='' small={true}/>
-
         <div className='container content-center blog-container'>
-          <div className='title'>
-            <h3>{this.state.post.title}</h3>
-          </div>
-          <div className='desc'>
-            <h5>{this.state.post.header}</h5>
-          </div>
-          <div className='author'>
-            <p>{this.state.post.author}</p>
-          </div>
-          <div className='body'>
-            <p>{this.state.post.body}</p>
-          </div>
+        {this.renderUpdateDelete()}
+        {this.renderPost()}
         </div>
       </div>
     );
