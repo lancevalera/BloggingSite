87f3615faa5f849d8ec1a3ee7068473585065d05
Lance Valera <lancevalera@Lances-MacBook-Pro.local>
2018-03-19 12:59:28 -0400

    added link

--- imports/ui/Home/card.js
+++ imports/ui/Home/card.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import {Link} from 'react-router-dom';
+
 export default class Cards extends Component {
 
   render() {
@@ -7,9 +9,13 @@ export default class Cards extends Component {
     return (
       <div className='card home-card'>
         <div className='card-body changemin'>
-          <h5 className="card-title">{this.props.body.title}</h5>
-          <p className='card-text description'>{this.props.body.header}</p>
-          <p className='card-text author'> {this.props.body.author} </p>
+          <Link to={'/post/'+this.props.body._id}>
+            <h5 className="card-title">{this.props.body.title}</h5>
+            <p className='card-text description'>{this.props.body.header}</p>
+          </Link>
+          <Link to={'/profile/'+this.props.body.authorID} >
+            <p className='card-text author'> {this.props.body.author} </p>
+          </Link>
         </div>
       </div>
     );
