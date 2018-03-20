ac1f2e482ebe865c4e29ca6a7b258324c731b57a
Lance Valera <lance.valera89@gmail.com>
2018-03-19 21:11:27 -0400

    removed style, formatted render html

--- imports/ui/LoginRegister/login.js
+++ imports/ui/LoginRegister/login.js
@@ -3,13 +3,11 @@ import {Link} from 'react-router-dom';
 
 import './login.css';
 
-const style = {
-  backgroundImage: "url(" + window.location.protocol + '//' +  window.location.host + '/images/login.jpeg'  + ")"
-}
-
-// App component - represents the whole app
+//represents the login form
 export default class Login extends Component {
 
+  //finds the user's profile based on the username and password given
+  //calls the onLogin function passed by the parent
   handleSignIn(e) {
     e.preventDefault();
     Meteor.call('user.find', this.refs.username.value, this.refs.password.value, (err, result) => {
@@ -30,24 +28,22 @@ export default class Login extends Component {
 
   render() {
     return (
-
-                <form className="form" onSubmit={this.handleSignIn.bind(this)}>
-                  <div className="content" style={{margin: 'auto'}}>
-                    <div className="input-group form-group-no-border input-lg">
-                      <input type="text" ref="username" placeholder="Username" className="form-control" style={{color: 'white'}} />
-                    </div>
-                    <div className="input-group form-group-no-border input-lg">
-                      <input type="password" ref="password" placeholder="Password" className="form-control" style={{color: 'white'}} />
-                    </div>
-                  </div>
-                  <div className="footer text-center submitbtn">
-                      <input type='submit' value='Login' className="btn btn-primary btn-round bluebtn" style={{width: '100%'}} />
-                  </div>
-                  <div className='pull-left'>
-                      <Link to='/register' className='nav-link' style={{color: 'white'}}><span> Create Account </span></Link>
-                  </div>
-                </form>
-
+          <form className="form" onSubmit={this.handleSignIn.bind(this)}>
+              <div className="content" style={{margin: 'auto'}}>
+                <div className="input-group form-group-no-border input-lg">
+                    <input type="text" ref="username" placeholder="Username" className="form-control" style={{color: 'white'}} />
+                </div>
+                <div className="input-group form-group-no-border input-lg">
+                    <input type="password" ref="password" placeholder="Password" className="form-control" style={{color: 'white'}} />
+                </div>
+              </div>
+              <div className="footer text-center submitbtn">
+                  <input type='submit' value='Login' className="btn btn-primary btn-round bluebtn" style={{width: '100%'}} />
+              </div>
+              <div className='pull-left'>
+                <Link to='/register' className='nav-link' style={{color: 'white'}}><span> Create Account </span></Link>
+              </div>
+          </form>
     );
   }
 }
