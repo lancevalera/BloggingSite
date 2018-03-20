05b7fbd6581b1c2b5723b78a1f24520229399d92
Lance Valera <lance.valera89@gmail.com>
2018-03-19 21:12:32 -0400

    added signout function

--- imports/ui/Navbar/navbarlinks.js
+++ imports/ui/Navbar/navbarlinks.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
+//represents the navbar links found on the right of the navbar
 export default class NavbarLinks extends Component {
+
+  //removes the user cookie, refresh the browser to get rid of state
+  signOut(e){
+    e.preventDefault();
+
+    const cookies = new Cookies();
+
+    cookies.remove('user');
+    window.location.reload();
+  }
 
   render() {
     return (
@@ -15,7 +27,7 @@ export default class NavbarLinks extends Component {
                 <Link to={'/profile/'+this.props.loggedIn._id} className="nav-link">Profile</Link>
             </li>
             <li className="nav-item">
-                <a href="#" className="nav-link">Sign out</a>
+                <a href="#" className="nav-link" onClick={this.signOut.bind(this)}>Sign out</a>
             </li>
             </ul>
             :
