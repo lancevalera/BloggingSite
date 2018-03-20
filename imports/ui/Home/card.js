af687a8ecadabed3bfe117c957161055e187dc30
Lance Valera <lance.valera89@gmail.com>
2018-03-19 21:10:49 -0400

    added comments, added classname to link

--- imports/ui/Home/card.js
+++ imports/ui/Home/card.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 import {Link} from 'react-router-dom';
 
+//represents the cards that can be seen in the home page
 export default class Cards extends Component {
 
   render() {
-    console.log(this.props);
     return (
       <div className='card home-card'>
         <div className='card-body changemin'>
-          <Link to={'/post/'+this.props.body._id}>
+          <Link to={'/post/'+this.props.body._id} className='title-description'>
             <h5 className="card-title">{this.props.body.title}</h5>
             <p className='card-text description'>{this.props.body.header}</p>
           </Link>
