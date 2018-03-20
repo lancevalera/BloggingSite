7da062627bb02b576d92f185eb9aed5766baffbe
Lance Valera <lance.valera89@gmail.com>
2018-03-19 21:09:06 -0400

    added comments, deleted console logs, changed if statement

--- imports/ui/BlogPost/blogpost.js
+++ imports/ui/BlogPost/blogpost.js
@@ -7,6 +7,7 @@ import Header from '../Header/header.js';
 import PostContent from './postcontent.js';
 import UpdateDelete from './updatedelete.js';
 
+//represents a detailed blog post
 export default class BlogPost extends Component {
   constructor(props){
     super(props);
@@ -16,6 +17,7 @@ export default class BlogPost extends Component {
     }
   }
 
+  //gets the blogpost using it's postId as the search parameter
   componentWillMount(){
     Meteor.call('posts.getById', this.props.router.match.params.postId, (err, result) => {
       if(err)
@@ -23,9 +25,9 @@ export default class BlogPost extends Component {
       else
         this.setState({post: result[0]});
     });
-    console.log(this.props);
   }
 
+  //deletes the post based on the post id
   deletePost(){
     Meteor.call('post.delete', this.state.post._id, (err, result) => {
       if(err)
@@ -35,6 +37,7 @@ export default class BlogPost extends Component {
     })
   }
 
+  //renders the post if the data is ready
   renderPost() {
     if(this.state.post)
       return <PostContent body={this.state.post.body} header={this.state.post.header} author={this.state.post.author}
@@ -43,9 +46,10 @@ export default class BlogPost extends Component {
       return null;
   }
 
+  //renders the update and delete buttons if the user and author are the same person
   renderUpdateDelete() {
     if(this.state.post && this.props.user){
-      if(this.state.post.authorID._str == this.props.user._id._str)
+      if(this.state.post.authorID == this.props.user._id)
         return (<div style={{width: '120%'}}> <UpdateDelete onDelete={this.deletePost.bind(this)} postId={this.state.post._id}/> </div>);
       else
         return null;
@@ -55,7 +59,6 @@ export default class BlogPost extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <div>
         <Header image='/images/profile.jpeg' textValue='' small={true}/>
