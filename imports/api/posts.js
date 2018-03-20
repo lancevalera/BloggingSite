87daf34d597eff619a7367a11cbb6557859d8bca
Lance Valera <lance.valera89@gmail.com>
2018-03-19 21:07:37 -0400

    add comments and sort option to queries

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -9,28 +9,34 @@ if (Meteor.isServer) {
 }
 
 Meteor.methods({
+  //gets all posts, ordering by date created
   'posts.getAll'(){
-    return Posts.find().fetch();
+    return Posts.find({}, {sort: {dateCreated: -1}}).fetch();
   },
 
+  //inserts a post, returning the postId
   'posts.insert'(newPost){
     var success = Posts.insert(newPost);
     return success;
   },
 
+  //returns a singlepost based on the unique Id
   'posts.getById'(id){
     return Posts.find({_id: id}).fetch();
   },
 
+  //deletes post
   'post.delete'(id){
     return Posts.remove({_id: id});
   },
 
+  //sets the post to the new values based on the id
   'post.update'(id, newPost){
     return Posts.update({_id: id}, newPost);
   },
 
+  //gets posts by author, ordering by date created
   'posts.getByUser'(id){
-    return Posts.find({authorID: id}).fetch();
+    return Posts.find({authorID: id}, {sort: {dateCreated: -1}}).fetch();
   }
 })
