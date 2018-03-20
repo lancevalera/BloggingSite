ea730f5e72f463839dc13e1a081fa2ee3facbfe2
Lance Valera <lance.valera89@gmail.com>
2018-03-19 21:12:25 -0400

    formatted navbar

--- imports/ui/Navbar/navbar.js
+++ imports/ui/Navbar/navbar.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import './navbar.css';
 
 import NavbarLinks from './navbarlinks.js';
-import DropdownLinks from './dropdownlinks.js'
 
-export default class navbar extends Component {
+//represents the navbar
+export default class Navbar extends Component {
 
   render() {
     return (
       <div>
       <nav className={"navbar navbar-expand-lg fixed-top " + (this.props.isLoggedIn ? 'bg-primary' : 'navbar-transparent')}>
         <div className="container">
-          <div className='dropdown button-dropdown'>
-            <DropdownLinks loggedIn={this.props.isLoggedIn} />
-        </div>
         <div className="navbar-translate">
-            <a className="navbar-brand">
+            <Link to='/' className="navbar-brand">
                BloggingSite
-           </a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-   	          <span className="navbar-toggler-bar bar1"></span>
-	            <span className="navbar-toggler-bar bar2"></span>
-	            <span className="navbar-toggler-bar bar3"></span>
- 	          </button>
+           </Link>
+
         </div>
 
         <div className="collapse navbar-collapse justify-content-end" id="navigation">
